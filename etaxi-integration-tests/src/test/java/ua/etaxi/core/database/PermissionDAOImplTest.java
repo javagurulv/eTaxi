abc9db83cfbd6652b827d8bfcb78ea9d49230670
javagurulv <javagurulv@gmail.com>
2014-10-11 08:39:11 +0300

    PermissionDAOImplTest

--- etaxi-integration-tests/src/test/java/ua/etaxi/core/database/PermissionDAOImplTest.java
+++ etaxi-integration-tests/src/test/java/ua/etaxi/core/database/PermissionDAOImplTest.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import ua.etaxi.core.domain.Permission;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -30,6 +32,69 @@ public class PermissionDAOImplTest extends DatabaseIntegrationTest {
                 assertThat(permission.getPermissionId(), is(nullValue()));
                 entityRepository.create(permission);
                 assertThat(permission.getPermissionId(), is(notNullValue()));
+            }
+        });
+    }
+
+    @Test
+    public void testGetById() {
+        final AtomicLong permissionId = new AtomicLong();
+        doInTransaction(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                Permission permission = new Permission();
+                permission.setPermissionName("admin");
+                entityRepository.create(permission);
+                permissionId.set(permission.getPermissionId());
+            }
+        });
+        doInTransaction(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                Permission permission = entityRepository.getById(Permission.class, permissionId.get());
+                assertThat(permission.getPermissionName(), is("admin"));
+            }
+        });
+    }
+
+    @Test
+    public void testGetById_EntityNotFound() {
+        doInTransaction(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                Permission permission = entityRepository.getById(Permission.class, Long.MAX_VALUE);
+                assertThat(permission, is(nullValue()));
+            }
+        });
+    }
+
+    @Test
+    public void testGetRequired() {
+        final AtomicLong permissionId = new AtomicLong();
+        doInTransaction(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                Permission permission = new Permission();
+                permission.setPermissionName("admin");
+                entityRepository.create(permission);
+                permissionId.set(permission.getPermissionId());
+            }
+        });
+        doInTransaction(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                Permission permission = entityRepository.getRequired(Permission.class, permissionId.get());
+                assertThat(permission.getPermissionName(), is("admin"));
+            }
+        });
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetRequired_EntityNotFound() {
+        doInTransaction(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                entityRepository.getRequired(Permission.class, Long.MAX_VALUE);
             }
         });
     }
